09b128b37705b85622dad75864a4bb3112d6bdb6
athuls8991@gmail.com <athuls8991@gmail.com>
2023-08-29 20:52:13 +0530

    change

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ console.log("Started");
 // });
 
 
+
 // const prom = new Promise((res,reg)=>{
 //     const pr2 = fetch("https://jsonplaceholder.typicode.com/todos/1").then(data =>data.json()).then((data)=>{
 //         res(data);
+
 //     }).catch((err)=>{
 //         reg("get Error");
 //     })
@@ -38,22 +40,34 @@ console.log("Started");
 // .then((data)=>{
 //     // console.log(data);
 // });
+// #########################################################
+
 const showTemp = document.createElement('h2');
+
 const weatherType = document.createElement('h6');
+
 const container = document.createElement('div');
+
 const img = document.createElement('IMG');
+
 const sun = document.createElement('h3');
+
 
 sun.classList.add('sunTime');
 img.classList.add('image');
+
 img.setAttribute('alt',"image");
+
+
 container.classList.add('content')
+
 showTemp.classList.add("showTemp");
+
 weatherType.classList.add('wt');
 
 
 //  const container = document.getElementById('container');
-
+// container.appendChild(sun);
 container.appendChild(img);
 container.appendChild(showTemp);
 container.appendChild(weatherType);
@@ -63,10 +77,11 @@ container.appendChild(sun);
 document.querySelector('#container').appendChild(container);
 
 navigator.geolocation.getCurrentPosition((pos)=>{
-    // let  = pos.coords;
+    console.log("pos"+pos);
+
     let lat = pos.coords.latitude;
     let lon = pos.coords.longitude;
-    // console.log(lat,lon);
+
     fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric&appid=bbe7b7cc65da9ff85e601d80e7e560de')
     .then((data)=>data.json()).then((data)=>{
 
@@ -82,7 +97,9 @@ navigator.geolocation.getCurrentPosition((pos)=>{
         showTemp.innerText = data.main.temp+" \xB0C";
         weatherType.innerText = weather.main;
     })
+
     
 })
+
 
 
